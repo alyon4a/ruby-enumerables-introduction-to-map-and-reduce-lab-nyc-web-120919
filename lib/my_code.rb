e6e110de6fc51a09041594afc92b24cbf8da19c2
Olena Matokhina <olena@matokhina.com>
2019-12-01 18:22:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,7 +38,7 @@ def map_to_square(source_array)
   return result
 end 
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total (source_array, starting_point)
   i = 0  
   puts "<><><>"
   puts source_array
